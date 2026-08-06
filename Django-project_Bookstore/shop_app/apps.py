d9899d31750eee5ae9698c9f64822e9722a18d16
Yevhen <minard1usz@gmail.com>
2026-08-06 22:15:54 +0300

    fix: auto run migrations on startup

--- Django-project_Bookstore/shop_app/apps.py
+++ Django-project_Bookstore/shop_app/apps.py
@@ -1,5 +1,19 @@
+import sys
 from django.apps import AppConfig
 
 
 class ShopConfig(AppConfig):
     name = "shop_app"
+
+class ShopAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shop_app"
+
+    def ready(self):
+        if "gunicorn" in sys.argv or "runserver" in sys.argv:
+            from django.core.management import call_command
+
+            try:
+                call_command("migrate", interactive=False)
+            except Exception as e:
+                print(f"Migration error on startup: {e}")
