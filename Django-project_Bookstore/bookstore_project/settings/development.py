516122ec9c5f3343baf2bea7337ce2e862305bd9
Yevhen <minard1usz@gmail.com>
2026-08-07 17:00:22 +0300

    docs: add build status and coverage badges to README

--- Django-project_Bookstore/bookstore_project/settings/development.py
+++ Django-project_Bookstore/bookstore_project/settings/development.py
@@ -24,8 +24,8 @@ DATABASES = {
 }
 
 # Debug Toolbar тільки для локалки
-INSTALLED_APPS += ["debug_toolbar"]
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+# INSTALLED_APPS += ["debug_toolbar"]
+# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
