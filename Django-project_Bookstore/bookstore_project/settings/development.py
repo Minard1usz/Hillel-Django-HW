91d9119407e6f3c3f79a605d6e93b3344c595e91
Yevhen <minard1usz@gmail.com>
2026-08-08 10:51:44 +0300

    fix: safely conditionally load debug_toolbar in urls.py based on INSTALLED_APPS

--- Django-project_Bookstore/bookstore_project/settings/development.py
+++ Django-project_Bookstore/bookstore_project/settings/development.py
@@ -24,11 +24,12 @@ DATABASES = {
 }
 
 # Debug Toolbar тільки для локалки
-# INSTALLED_APPS += ["debug_toolbar"]
-# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
+    "localhost",
 ]
 
 CORS_ALLOWED_ORIGINS = [
--- Django-project_Bookstore/bookstore_project/urls.py
+++ Django-project_Bookstore/bookstore_project/urls.py
@@ -50,7 +50,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    import debug_toolbar
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
 
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
